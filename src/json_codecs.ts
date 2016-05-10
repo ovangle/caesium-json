5210055daba95ec176dee49aa62fb5fa40592624
Thomas Stephenson <ovangle@gmail.com>
2016-05-10 12:26:56 +1000

    Export {JsonObject, JsonQuery, Codec, Converter, identity} from json_codecs
    They are interfaces which are commonly required when using codecs

--- src/json_codecs.ts
+++ src/json_codecs.ts
@@ -1,3 +1,7 @@
+export {Codec, identity} from 'caesium-core/codec';
+export {Converter} from 'caesium-core/converter';
+
+export {JsonObject,JsonQuery} from './json_codecs/interfaces';
 export {str, num, bool, date, list, map} from './json_codecs/basic';
 export {model} from './json_codecs/model_to_json';
 export {union} from './json_codecs/model_union_to_json';
