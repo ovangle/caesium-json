bddfd341b00941559908e7be126951e367db2a87
Thomas Stephenson <ovangle@gmail.com>
2016-11-18 11:42:47 +1100

    Remove recordCodec from json_codecs.
    We want to be able to use Models to fulfill the role that Records
    have in implementation.

--- src/json_codecs/index.ts
+++ src/json_codecs/index.ts
@@ -4,6 +4,6 @@ export {JsonObject} from './interfaces';
 export {error, str, num, bool, date, dateTime, list, map} from './basic';
 export {enumToString} from './enum_to_string';
 export {itemList} from './item_list';
+export {objectToJson, jsonToObject} from './object_to_json';
 export {model} from './model_to_json';
 export {union} from './model_union_to_json';
-export {recordCodec} from './record_to_json';
