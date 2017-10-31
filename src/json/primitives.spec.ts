2cc2afbee1dd78484dea387cc4e5fe7284b47161
Thomas Stephenson <ovangle@gmail.com>
2017-10-31 13:32:41 +1100

    Remove moment peer dependency

--- src/json/primitives.spec.ts
+++ src/json/primitives.spec.ts
@@ -1,7 +1,7 @@
 
 import {List, Map} from 'immutable';
 import {Codec} from '../codec';
-import {str, bool, num, date, dateTime} from './primitives';
+import {str, bool, num, date} from './primitives';
 
 export function expectThrowsOnNullOrUndefined(codec: Codec<any,any>) {
     expect(() => codec.encode(null)).toThrow(jasmine.any(Error));
@@ -44,25 +44,14 @@ describe('primitives', () => {
         })
     });
 
-  describe('dateTime', () => {
-    it('should be able to encode and decode blank values', () => {
-      expectThrowsOnNullOrUndefined(dateTime);
-    });
-
-    it('should be able to encode/decode a date', () => {
-      expect(dateTime.encode(new Date(0))).toBe('1970-01-01T00:00:00.000Z');
-      expect(dateTime.decode('1970-01-01T00:00:00Z')).toEqual(new Date(0));
-    });
-  });
-
   describe('date', () => {
     it('should be able to encode and decode blank values', () => {
       expectThrowsOnNullOrUndefined(date);
     });
 
-    it('should be able to encode a date in the format YYYY-MM-DD', () => {
-      expect(date.encode(new Date(0))).toBe('1970-01-01');
-      expect(date.decode('1970-01-01')).toEqual(new Date(0));
+    it('should be able to encode/decode a date', () => {
+      expect(date.encode(new Date(0))).toBe('1970-01-01T00:00:00.000Z');
+      expect(date.decode('1970-01-01T00:00:00Z')).toEqual(new Date(0));
     });
   });
 });
